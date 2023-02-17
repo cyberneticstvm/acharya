7cdf12d305074451b3d30e1817ba667ceec13570
Vijoy Sasidharan <devuinfosys@gmail.com>
2023-02-17 14:39:20 +0530

    small updates

--- resources/views/reports/student.blade.php
+++ resources/views/reports/student.blade.php
@@ -6,6 +6,16 @@
             <div class="card-header pb-0 text-left bg-transparent">
                 <h3 class="font-weight-bolder text-primary text-gradient">Fetch Student</h3>
             </div>
+            @if(session()->has('success'))
+                <div class="alert alert-success text-white">
+                    {{ session()->get('success') }}
+                </div>
+            @endif
+            @if(session()->has('error'))
+                <div class="alert alert-danger text-white">
+                    {{ session()->get('error') }}
+                </div>
+            @endif
             <div class="card-body">
                 <form role="form" method="post" action="{{ route('report.student.fetch') }}">
                     @csrf
